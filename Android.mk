20ed6ef0a58a0aeb222019699651e70bad45a833
Kenneth Endfinger <kaendfinger@gmail.com>
2014-11-09 23:08:13 -0500

    ugh

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_SRC_FILES += \
-  src/main/aidl/org/directcode/neo/sea/SeaController.aidl
+  src/org/directcode/neo/sea/SeaController.aidl
 
 LOCAL_SDK_VERSION := current
 LOCAL_PACKAGE_NAME := Sea
