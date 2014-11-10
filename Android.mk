ea0b7d2d105dfca61b789faacd03b8b89f61ae29
Kenneth Endfinger <kaendfinger@gmail.com>
2014-11-09 21:54:42 -0500

    Bug Fixes

--- Android.mk
+++ Android.mk
@@ -1,7 +1,6 @@
 LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4, guava
+include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
