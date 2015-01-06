ac4b56fc274263530f36c5e98e19eec0a3ff4070
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-05 22:45:07 -0500

    Added BatteryIconPercentage to normal modules

--- src/org/directcode/neo/sea/SeaUtils.java
+++ src/org/directcode/neo/sea/SeaUtils.java
@@ -1,11 +1,11 @@
 package org.directcode.neo.sea;
 
-import android.provider.Settings;
-
+import org.directcode.neo.sea.modules.BatteryIconPercentage;
 import org.directcode.neo.sea.modules.VolumeMusicController;
 
 public class SeaUtils {
     public static void applyModules(Sea sea) {
         sea.addModule(new VolumeMusicController());
+        sea.addModule(new BatteryIconPercentage());
     }
 }
