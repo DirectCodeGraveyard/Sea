a86f1ea0890a7af2fd8aa613e0206ad6f13070d8
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-05 23:18:12 -0500

    Disable the Battery Percentage Icon for now

--- src/org/directcode/neo/sea/SeaUtils.java
+++ src/org/directcode/neo/sea/SeaUtils.java
@@ -6,6 +6,6 @@ import org.directcode.neo.sea.modules.VolumeMusicController;
 public class SeaUtils {
     public static void applyModules(Sea sea) {
         sea.addModule(new VolumeMusicController());
-        sea.addModule(new BatteryIconPercentage());
+        // sea.addModule(new BatteryIconPercentage());
     }
 }
