6bbea341e4ac9fde7aa79b5d707671a1d34a499f
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-10 14:35:52 -0500

    Bug Fix

--- src/org/directcode/neo/sea/SeaUtils.java
+++ src/org/directcode/neo/sea/SeaUtils.java
@@ -1,11 +1,9 @@
 package org.directcode.neo.sea;
 
-import org.directcode.neo.sea.modules.BatteryIconPercentage;
 import org.directcode.neo.sea.modules.VolumeMusicController;
 
 public class SeaUtils {
     public static void applyModules(Sea sea) {
         sea.addModule(new VolumeMusicController());
-        // sea.addModule(new BatteryIconPercentage());
     }
 }
