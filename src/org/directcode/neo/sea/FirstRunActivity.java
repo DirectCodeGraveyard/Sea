1716029b4763c25fd39e5fb02e37f9002efeabe8
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-11 15:17:18 -0500

    Refactoring Simple Things

--- src/org/directcode/neo/sea/FirstRunActivity.java
+++ src/org/directcode/neo/sea/FirstRunActivity.java
@@ -5,13 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-
 public class FirstRunActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first_run);
+
+        setContentView(R.layout.first_run);
         Button getStartedButton = (Button) findViewById(R.id.getStartedButton);
         getStartedButton.setOnClickListener(new View.OnClickListener() {
             @Override
